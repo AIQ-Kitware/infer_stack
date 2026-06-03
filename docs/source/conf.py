5c1ed3e503d662357af9340a2687f28752e68ca7
jon.crall <jon.crall@kitware.com>
2026-06-03 11:59:13 -0400

    Add github actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -140,8 +140,8 @@ def parse_version(fpath):
 
 
 project = 'infer_stack'
-copyright = '2026, jon.crall'
-author = 'jon.crall'
+copyright = '2026, Jon Crall'
+author = 'Jon Crall'
 modname = 'infer_stack'
 
 repo_dpath = dirname(dirname(dirname(__file__)))
@@ -381,7 +381,7 @@ latex_documents = [
         master_doc,
         'infer_stack.tex',
         'infer_stack Documentation',
-        'jon.crall',
+        'Jon Crall',
         'manual',
     ),
 ]
